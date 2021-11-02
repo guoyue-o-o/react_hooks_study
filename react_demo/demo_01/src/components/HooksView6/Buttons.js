14c9e400e79fdec936068603ffb72986eaa1daed
guoyue <guoyue@eastsoft.com.cn>
2021-11-02 20:30:56 +0800

    'hooks学习'

--- react_demo/demo_01/src/components/HooksView6/Buttons.js
+++ react_demo/demo_01/src/components/HooksView6/Buttons.js
@@ -1,15 +1,16 @@
-import React from 'react';
+import React, { useContext, } from 'react';
+import { ColorContext, UPDATE_COLOR, } from './color';
 
 function Buttons() {
-
+    const { dispatch } = useContext(ColorContext)
     return (
         <div style={{
             color: 'blue'
         }}>
-            <button>
+            <button onClick={() =>{dispatch({type: UPDATE_COLOR, color: 'red'})}}>
                 红色
             </button>
-            <button>
+            <button onClick={() =>{dispatch({type: UPDATE_COLOR, color: 'yellow'})}}>
                 黄色
             </button>
         </div>
--- react_demo/demo_01/src/components/HooksView6/color.js
+++ react_demo/demo_01/src/components/HooksView6/color.js
@@ -1,11 +1,26 @@
-import React, { createContext } from 'react';
+import React, { createContext, useReducer } from 'react';
 
 export const ColorContext = createContext({})
 
+export const UPDATE_COLOR = 'UPDATE_COLOR';
+
+const reducer = (state, action) => {
+    switch (action.type) {
+        case UPDATE_COLOR:
+            return action.color;
+        default:
+            return state;
+    }
+}
+
 // props当我们有子组件的时候
 export const Color = props => {
+    // useReducer(参数1，参数2)
+    // 参数1 是方法
+    // 参数2 是默认值
+    const [ color, dispatch ] =  useReducer(reducer, 'blue')
     return(
-        <ColorContext.Provider value={{color: 'red'}}>
+        <ColorContext.Provider value={{color, dispatch}}>
             {props.children}
         </ColorContext.Provider>
     )
